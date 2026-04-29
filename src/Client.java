83a585bbe8a2aef3d3f7f8c24e05e18a64744326
md37971 <md37971@fortsmithschools.org>
2026-04-29 14:15:57 -0500

    Cleaned Class Files and Fixed Client.

--- src/Client.java
+++ src/Client.java
@@ -5,12 +5,21 @@ public class Client{
     private Socket socket;
     private BufferedReader br;//input
     private PrintWriter pw;//output
-    private boolean hasRoom = false; //whether the client has a fitting room or not -AE
+    private boolean hasRoom = false;
 
-    public Client(String serverIP, int port){
+    public int port;
+    public int clientID; //This was private.
+    public String serverIP;
+
+    public Client(String serverIP, int port, int clientID){
+        this.serverIP = serverIP;
+        this.port = port;
+        this.clientID = clientID;
+
         try{
             socket = new Socket(serverIP,port);
-            System.out.println("Connected to Server");
+            //display("connected to server");
+            System.out.println("Connected to server");
             br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             pw = new PrintWriter(socket.getOutputStream(),true);
 
@@ -18,9 +27,8 @@ public class Client{
             new ServerListener().start();
         }catch(IOException e){
             e.printStackTrace();
-            System.out.println("Unable to connect to Central Server");
-            return; 
-            
+            System.out.println("Unable to connect to central serer");
+            return;
         }
     }
 
@@ -30,61 +38,69 @@ public class Client{
                 String response;
                 while((response = br.readLine()) != null){
                     responseHandler(response);
+                    
                 }
 
             }catch(IOException e){
-                System.out.println("Disconnected from Server");
+                display("disconnected from server");
             }
         }
     }
 
+    private void display(String message){
+        System.out.println("Customer #" + clientID + " " + message + " <Server: " + serverIP + ">");
+    }
+
     private void responseHandler(String response){
+
         System.out.println("Server: " + response);
-
-        if (response.startsWith("Room Allocated")) {
+        if (response.equals("Room Allocated")) {
             hasRoom = true;
-            System.out.println("Entering fitting room...");
+            display("leaves waiting area and enters fitting room");
             simulateFittingRoomUse();
-            //What happens if the client is given a fitting room
 
-        } else if (response.startsWith("ROOMS FULL")) {
-            System.out.println("ROOMS FULL. Waiting for available fitting room...");
-            //What happens if a client is in the waiting room because all fitting rooms are full
+        } else if (response.equals("Wait")) {
+
+            display("enters the waiting area and takes a seat");
 
         } else if (response.equals("Room Available")) {
-            System.out.println("Room now available, requesting again...");
+
+            display("notified that a fitting room is available");
             requestFittingRoom();
-            //what happens when rooms are available
 
         } else {
-            System.out.println("Come back later");
-            //what happens if waiting room and fitting rooms are all full
+
+            display("leaves the store (no space available)");
+            exit();
         }
-    }
+}   
 
     //When client is trying to get into a fitting room
     public void requestFittingRoom(){
-        if(pw == null){
-            System.out.println("CLIENT: Not connected - cannot send"); 
+        if(pw == null ) {
+            System.out.println("Client: Not Connected - Cannot Send.");
             return;
         }
         pw.println("Request Room");
-        System.out.println("Requested fitting room");
+        display("requests a fitting room");
     }
 
     //When client is done with fitting room
     public void releaseFittingRoom(){
-        if(hasRoom){
+        if(hasRoom) {
             pw.println("Release Room");
             hasRoom = false;
-            System.out.println("Released Fitting Room");
-        }
+            System.out.println("Released Fitting Room.");
+            //display("leaves fitting room");
+        }    
     }
 
     //When client no longer wants to contact central server
     public void exit(){
         pw.println("Exit");
         try{
+            pw.close();
+            br.close();
             socket.close();
         }catch(IOException e){
             e.printStackTrace();
@@ -93,7 +109,7 @@ public class Client{
 
     private void simulateFittingRoomUse(){
     	try {
-    		int sleepTime = (int)(Math.random() * 5000);
+    		int sleepTime = (int)(Math.random() * 1000);
     		Thread.sleep(sleepTime);
 
     		releaseFittingRoom();
@@ -106,19 +122,34 @@ public class Client{
 
     
     public static void main(String[] args) {
+        if (args.length < 1) {
+        System.out.println("Usage: java Client <number_of_fitting_rooms>");
+        return;
+    }
+
+        int fittingRooms = Integer.parseInt(args[0]);
+        int chairs = fittingRooms * 2;
+        int totalCustomers = fittingRooms + chairs;
+        
         String serverIP = "127.0.0.1";
-    	int port = 50000;
+    	int port = 50001;
 
     	int clientId = 1;
 
-    	while (true) {
+        System.out.println("Using arguments from command line");
+        System.out.println("Fitting Rooms = " + fittingRooms);
+        System.out.println("Number of chairs in the waiting area = " + chairs);
+        System.out.println("Number of customers = " + totalCustomers);
+
+        //while(true) {
+    	while (clientId <= totalCustomers) {
         	int id = clientId++;
 
     		new Thread(new Runnable() {
     			@Override
     			public void run() {
-        			Client client = new Client(serverIP, port);
-        			System.out.println("Customer #" + id + " enters the system");
+        			Client client = new Client(serverIP, port, id);
+        			client.display("enters the system");
         			client.requestFittingRoom();
     			}
 			}).start();
