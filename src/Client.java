c48b7e96c517c745c5b416890447a61f13afb89e
sgiles01 <sgiles01@uafs.edu>
2026-05-05 17:13:32 -0500

    updated client to except arg for num of clients

--- src/Client.java
+++ src/Client.java
@@ -2,21 +2,24 @@ import java.io.*;
 import java.net.*;
 
 public class Client{
-    private Socket socket;
+    public Socket socket;
     private BufferedReader br;//input
     private PrintWriter pw;//output
+    private boolean hasRoom = false;
 
-    //test
-    private int clientID;
-    private String serverIP;
+    public int port;
+    public int clientID; //This was private.
+    public String serverIP;
 
     public Client(String serverIP, int port, int clientID){
         this.serverIP = serverIP;
+        this.port = port;
         this.clientID = clientID;
 
         try{
             socket = new Socket(serverIP,port);
-            display("connected to server");
+            //display("connected to server");
+            System.out.println("Connected to server");
             br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             pw = new PrintWriter(socket.getOutputStream(),true);
 
@@ -24,6 +27,8 @@ public class Client{
             new ServerListener().start();
         }catch(IOException e){
             e.printStackTrace();
+            System.out.println("Unable to connect to central serer");
+            return;
         }
     }
 
@@ -31,12 +36,19 @@ public class Client{
         public void run(){
             try{
                 String response;
-                while((response = br.readLine()) != null){
+                while(socket !=null && !socket.isClosed() && (response = br.readLine()) != null){
                     responseHandler(response);
+                    
                 }
 
             }catch(IOException e){
-                display("disconnected from server");
+                if(socket != null && socket.isClosed()){
+                    System.out.println("Client " + clientID + " listener stopped (Socket closed)");
+                }else{
+                    System.out.println("Error in client listener");
+                    e.printStackTrace();
+                }
+               
             }
         }
     }
@@ -47,12 +59,13 @@ public class Client{
 
     private void responseHandler(String response){
 
-        if (response.equals("Room Allocated")) {
-
+        System.out.println("Server: " + response);
+        if (response.startsWith("Room Allocated")) {
+            hasRoom = true;
             display("leaves waiting area and enters fitting room");
             simulateFittingRoomUse();
 
-        } else if (response.equals("Wait")) {
+        } else if (response.startsWith("Wait")) {
 
             display("enters the waiting area and takes a seat");
 
@@ -61,23 +74,33 @@ public class Client{
             display("notified that a fitting room is available");
             requestFittingRoom();
 
-        } else {
-
+        } else if(response.equals("Full")) {
             display("leaves the store (no space available)");
             exit();
+         }else{
+
+          System.out.println("Unknown response :" + response);
         }
 }   
 
     //When client is trying to get into a fitting room
     public void requestFittingRoom(){
-        pw.println("Request Room");
+        if(pw == null ) {
+            System.out.println("Client: Not Connected - Cannot Send.");
+            return;
+        }
+        pw.println("Request Room " + clientID); 
         display("requests a fitting room");
     }
 
     //When client is done with fitting room
     public void releaseFittingRoom(){
-        pw.println("Release Room");
-        display("leaves fitting room");    
+        if(hasRoom) {
+            pw.println("Release Room " + clientID);
+            hasRoom = false;
+            System.out.println("Released Fitting Room.");
+            display("leaves fitting room");
+        }    
     }
 
     //When client no longer wants to contact central server
@@ -94,10 +117,16 @@ public class Client{
 
     private void simulateFittingRoomUse(){
     	try {
-    		int sleepTime = (int)(Math.random() * 1000);
+    		int sleepTime = (int)(Math.random() * 5000);
     		Thread.sleep(sleepTime);
 
     		releaseFittingRoom();
+
+            //adding a tiny delay to prevent msg getting lost before disconnect. 
+            try{
+                Thread.sleep(50);
+            }catch(InterruptedException e){  }
+            
     		exit();
 
 		} catch (InterruptedException e) {
@@ -112,7 +141,7 @@ public class Client{
         return;
     }
 
-        
+    
         int totalCustomers = Integer.parseInt(args[0]);
         
         String serverIP = "127.0.0.1";
@@ -120,10 +149,6 @@ public class Client{
 
     	int clientId = 1;
 
-        //System.out.println("Using arguments from command line");
-        //System.out.println("Fitting Rooms = " + fittingRooms);
-        //System.out.println("Number of chairs in the waiting area = " + chairs);
-        //System.out.println("Number of customers = " + totalCustomers);
 
     	while (clientId <= totalCustomers) {
         	int id = clientId++;
@@ -134,17 +159,26 @@ public class Client{
         			Client client = new Client(serverIP, port, id);
         			client.display("enters the system");
         			client.requestFittingRoom();
-    			}
-			}).start();
+
+                    //KEEP CLIENT ALIVE UNTIL IT EXITS ON ITS OWN. 
+                    //OTHERWISE, MAIN THREAD WILL EXIT AND CLOSE CONNECTION. 
+                    try{
+                        while(client.socket != null && !client.socket.isClosed()){
+                            Thread.sleep(100);
+                        }
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
+			}}).start();
 
         	try {
             	Thread.sleep((int)(Math.random() * 1000));//this is the delay for the random entrance time
         	} catch (InterruptedException e) {
             	e.printStackTrace();
         	}
+
+        }
+        
     	}
 
 	}
-
-   
-}
